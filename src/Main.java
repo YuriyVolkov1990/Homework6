119e052f717fbd92aa0bb51ccebe82b3cd890129
Professional <new99kpicem@gmail.com>
2023-02-19 19:57:41 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+    }
+
+    public static void task1() {
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(+i);
+        }
+    }
+}
