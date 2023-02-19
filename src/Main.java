ec127d26fc3a257090beb3dded77458c94e93243
Professional <new99kpicem@gmail.com>
2023-02-19 20:17:30 +0300

    seven tasks

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
     }
 
     public static void task1() {
@@ -37,6 +39,18 @@ public class Main {
         System.out.println("Задача 5");
         for (int i = 1904; i <= 2096; i = i + 4){
             System.out.println("Високосный год " + i);
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7){
+            System.out.println(+ i);
+        }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2){
+            System.out.println(+ i);
         }
     }
 }
